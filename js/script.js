d64374226e6a0453c8339a35fc7a35da11a8eb65
marcocareddu <saldumaccu@tuta.io>
2023-05-31 15:53:43 +0200

    JS Mail - Added user input

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ console.log('JS OK')
 // *PHASE 1
 // Create array
 const mailList = ['pistone@bikermice.it', 'sterzo@bikermice.it', 'turbo@bikermice.it'];
+
+// Ask user's email
+const userInput = prompt('Qual è la tua email?');
+
