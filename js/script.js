14076b10faccc7170d826770e16d5a39803d6d7b
marcocareddu <saldumaccu@tuta.io>
2023-05-31 16:24:35 +0200

    JS Mail - Added FOR cicle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 console.log('JS OK')
 
+
+/**************************************************/
+/**               ESERCIZIO EMAIL                **/
+/**************************************************/
+
 // *PHASE 1
 // Create array
 const mailList = ['pistone@bikermice.it', 'sterzo@bikermice.it', 'turbo@bikermice.it'];
 
 // Ask user's email
-const userInput = prompt('Qual è la tua email?');
+const userInput = prompt('Qual è la tua email?').trim();
+let isValid = '';
+
+// *PHASE 2
+// FOR cicle 
+for (let i = 0; i < mailList.length; i++) {
+    console.log(mailList[i]);
+    if (userInput === mailList[i]) {
+        isValid = true;
+    }
+}
 
